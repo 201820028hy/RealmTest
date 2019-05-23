d69718b91ac77a7427d292fd40a7d269f9d0f44b
dit03 <dit@imac-4.local>
2019-05-23 12:31:59 +0900

    Use Realm

--- RealmTest/ViewController.swift
+++ RealmTest/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Person: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textValue: UITextField!
+    @IBOutlet weak var textAge: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnAdd(_ sender: Any) {
+        let person = Person()
+        
+        person.name = textValue.text!
+        person.age = Int(textAge.text!)!
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(person)
+        }
+        textValue.text = ""
+        textAge.text = ""
+    }
+    
+    @IBAction func btnDeleteAll(_ sender: Any) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+    
+    @IBAction func btnGet(_ sender: Any) {
+        textView.text = ""
+        
+        let realm = try! Realm()
+        let vals = realm.objects(Person.self)//불러온다
+        
+        if vals.count == 0 {
+            return
+        } else {
+            for i in vals {
+                textView.text = textView.text! + "Name : \(i.name), Age : \(i.age) \n"
+            }
+        }
+    }
+    
 }
 
